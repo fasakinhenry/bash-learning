a2ff81ea50c3b05c1ce6bc6c89acbcd9552e7ff0
Fasakin Henry <fasakinhenry@gmail.com>
2024-07-29 03:08:26 +0100

    Improve the typing animation

--- src/components/TypingEffect/index.js
+++ src/components/TypingEffect/index.js
@@ -1,38 +1,65 @@
 import React, { useEffect, useState } from 'react';
-import './TypingEffect.css'; // Make sure to import the CSS file
+import './TypingEffect.css';
 
-const TypingEffect = ({ textArray, typingSpeed = 100, deletingSpeed = 50, delay = 5000 }) => {
+const TypingEffect = ({
+  textArray,
+  typingSpeed = 100,
+  deletingSpeed = 50,
+  delay = 2000, // Delay before deleting (in milliseconds)
+}) => {
   const [displayedText, setDisplayedText] = useState('');
   const [index, setIndex] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     let timer;
     const currentText = textArray[index];
-    if (isDeleting) {
+
+    if (isPaused) {
       timer = setTimeout(() => {
-        setDisplayedText(currentText.substring(0, displayedText.length - 1));
-        if (displayedText.length === 0) {
-          setIsDeleting(false);
-          setIndex((prevIndex) => (prevIndex + 1) % textArray.length);
-        }
-      }, deletingSpeed);
-    } else {
-      timer = setTimeout(() => {
-        setDisplayedText(currentText.substring(0, displayedText.length + 1));
-        if (displayedText === currentText) {
-          setIsDeleting(true);
-        }
-      }, typingSpeed);
+        setIsPaused(false);
+        setIsDeleting(true);
+      }, delay);
+    } else if (isDeleting) {
+      if (displayedText.length === 0) {
+        setIsDeleting(false);
+        setIsTyping(true);
+        setIndex((prevIndex) => (prevIndex + 1) % textArray.length);
+      } else {
+        timer = setTimeout(() => {
+          setDisplayedText(displayedText.slice(0, -1));
+        }, deletingSpeed);
+      }
+    } else if (isTyping) {
+      if (displayedText === currentText) {
+        setIsTyping(false);
+        setIsPaused(true);
+      } else {
+        timer = setTimeout(() => {
+          setDisplayedText(currentText.slice(0, displayedText.length + 1));
+        }, typingSpeed);
+      }
     }
 
     return () => clearTimeout(timer);
-  }, [displayedText, isDeleting, index, textArray, typingSpeed, deletingSpeed]);
+  }, [
+    displayedText,
+    isTyping,
+    isDeleting,
+    isPaused,
+    index,
+    textArray,
+    typingSpeed,
+    deletingSpeed,
+    delay,
+  ]);
 
   return (
-    <div className="typing-effect">
+    <div className='typing-effect'>
       {displayedText}
-      <span className="cursor">|</span>
+      <span className='cursor'>|</span>
     </div>
   );
 };
